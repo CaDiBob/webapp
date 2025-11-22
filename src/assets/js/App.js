5af1f8fa72d700373da7f77c780e178be05e5573
CaDiBob <nikolay.v.byankin@gmail.com>
2025-11-22 15:43:19 +0900

    Update

--- src/assets/js/App.js
+++ src/assets/js/App.js
@@ -1,3 +1,4 @@
+/** app init class **/
 const config = {
     real_platform: null,
     platform: null,
@@ -8,14 +9,16 @@ const config = {
     switch_on: true,
     send_data_status: 'fetching...',
     get_status: 'fetching...',
-    links: [],
+    links: {},
 };
 
+// https://bill.ssvpnapp.win/devise_webapp_api
 class App {
+    /** create app **/
     constructor() {
         this.init();
     }
-
+    /** init other modules **/
     async init() {
         this.initData();
         window.addEventListener('load', () => {
@@ -38,9 +41,10 @@ class App {
     initData() {
         const url_param = new URLSearchParams(location.search);
         const os_data = getOS();
+        console.log(window.location);
 
-        config.platform = url_param.get('platform') || os_data.app_platform;
-        config.protocol = url_param.get('def_protocol') || 'ss';
+        config.platform = url_param.get('platform') ? url_param.get('platform') : os_data.app_platform;
+        config.protocol = url_param.get('def_protocol') ? url_param.get('def_protocol') : 'ss';
         config.switch_on = (url_param.get('switch_off') === null || url_param.get('switch_off') === 'false');
         config.telegram_id = url_param.get('telegram_id');
         config.zone_id = url_param.get('zone_id');
@@ -49,42 +53,52 @@ class App {
     }
 
     async initLinkApi() {
-        const links = [
-            {
-                app_name: 'foxray',
-                platform: 'ios',
-                protocol: 'vless',
-                link: 'https://example.com/foxray'
-            },
-            {
-                app_name: 'outline',
-                platform: 'all',
-                protocol: 'ss',
-                link: 'https://example.com/outline'
-            },
-            {
-                app_name: 'streisand',
-                platform: 'android',
-                protocol: 'vless',
-                link: 'https://example.com/streisand'
-            }
-        ];
+        const url = new URL(location.origin);
+        url.pathname = '/client/subscription';
+        url.search = new URLSearchParams({
+            telegram_id: config.telegram_id,
+            zone_id: config.zone_id,
+        }).toString();
+        const links = await fetch(url)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('Network response was not OK');
+                }
+                config.get_status = 'ok';
+                return res.json();
+            })
+            .catch((error) => {
+                config.get_status = 'error';
+                console.log(error);
+            });
 
-        config.links = links;
+        config.links = {
+            ss: links.configs.outline.link,
+            vless: links.configs.http_vless.link,
+            hiddify: links.configs.raw_vless.link,
+            foxray: links.configs.foxray.link,
+            v2raytun_android: links.configs.v2raytun_android.link,
+            streisand: links.configs.streisand.link,
+            v2raytun: links.configs.v2raytun.link,
+        };
+
         config.get_status = 'ok';
-        return links;
+
+        return config.links;
     }
 
+    // async initLinkApi() {
+    //
+    // }
 
     async getUserIp() {
         return fetch('https://api.ipify.org/')
             .then((response) => response.text())
             .catch((error) => console.log(error));
     }
-
     async sendData() {
         const res = await this.getUserIp();
-        const ip = res || 'not_set';
+        const ip = (res) ? res : 'not_set';
 
         const devise = {
             ip: ip,
@@ -93,7 +107,6 @@ class App {
             language: navigator.language,
             platform: navigator.platform,
         };
-
         return fetch('/devise_webapp_api', {
             method: 'POST',
             body: JSON.stringify(devise),
@@ -102,14 +115,16 @@ class App {
             },
         })
             .then((res) => {
-                if (!res.ok) throw new Error('Network response was not OK');
+                if (!res.ok) {
+                    throw new Error('Network response was not OK');
+                }
                 config.send_data_status = 'ok';
             })
             .catch((error) => {
                 config.send_data_status = 'error';
                 console.log(error);
             });
-    }
+    };
 }
 
 new App();
